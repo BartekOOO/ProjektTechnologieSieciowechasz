4915cab7b1d3f78fd0061e9dc0196326e5e7617d
Bartek Władimiruk <rojdanbw@gmail.com>
2024-12-01 14:55:36 +0100

    w pełni działajace konwersacja pomiędzy dwoma użytkownikami na czacie ogólnym

--- Server/Server.cs
+++ Server/Server.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
+using TechnologieSiecioweLibrary.Models;
 using TechnologieSiecioweLibrary.Services;
 
 class Server
@@ -47,6 +48,38 @@ class Server
                 string result = await ControllerService.ProcessData(message);
 
                 Console.WriteLine("Wysłano wiadomość: "+result);
+
+                try
+                {
+                    ResponseData<Message> data = new ResponseData<Message>();
+                    data.ReadDataFromJSON(result);
+                    if(data.Data.ReceiverId == 0)
+                    {
+                        lock (clients)
+                        {
+                            foreach (var clientStream in clients)
+                            {
+                                try
+                                {
+                                    if (clientStream != client.GetStream())
+                                    {
+                                        StreamWriter clientWriter = new StreamWriter(clientStream) { AutoFlush = true };
+                                        clientWriter.WriteLineAsync(result);
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("Błąd wysyłania do klienta: " + ex.Message);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+
 
                 await writer.WriteLineAsync(result);
             }
